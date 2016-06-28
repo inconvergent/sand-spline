118e3cfcbe16b4e678e42bc1b3ea981df212ae0d
Anders Hoff <inconvergent@gmail.com>
2016-06-29 00:34:36 +0200

    working grid

--- main-guided.py
+++ main-guided.py
@@ -5,7 +5,8 @@ from __future__ import print_function
 from __future__ import division
 
 from numpy import pi
-from numpy.random import random
+from numpy.random import randint
+
 from numpy import linspace
 from numpy import zeros
 from numpy import ones
@@ -17,7 +18,7 @@ BACK = [1,1,1,1]
 FRONT = [0,0,0,0.05]
 LIGHT = [0,0,0,0.05]
 
-SIZE = 2000
+SIZE = 3000
 PIX = 1.0/SIZE
 
 EDGE = 0.08
@@ -27,51 +28,53 @@ NOISE_STP = 0.005
 
 GRAINS = 1
 
-GRID_SIZE = 6
+GRID_SIZE = 30
 
 GNUM = 5
-PNUM = 5
-
-INUM = 500
-STEPS = 500
-N = 10
 
 GUIDE_NOISE = 0.05
-STP = 0.00005
+STP = 0.000008
+
+INUM = 300
 
 def draw(render, xy):
   points = column_stack((xy[1:,:], xy[:-1,:]))
   render.sandstroke(points,GRAINS)
 
-
 def spline_iterator():
   from modules.guidedSandSpline import GuidedSandSpline
 
-  i = 0
-  leap = (1.0-2*EDGE)/(GRID_SIZE-1)*0.5*0.5
+  itt = 0
+  leap = (1.0-2*EDGE)/(GRID_SIZE-1)*0.5*0.75
   print('leap', leap)
 
   for i, a in enumerate(linspace(EDGE, 1.0-EDGE, GRID_SIZE)):
     for j, b in enumerate(linspace(EDGE, 1.0-EDGE, GRID_SIZE)):
-      i += 1
+      itt += 1
       gx = linspace(a-leap,a+leap,GNUM,'float')
       gy = b*ones(GNUM,'float')
-      # gy = linspace(b-leap,b+leap,PNUM,'float')
-      # gy = b+sorted((1.0-2.0*random(size=GNUM))*GUIDE_NOISE)
       guide = column_stack((gx, gy))
 
-      px = zeros(PNUM,'float')
-      py = linspace(-1,1,PNUM,'float')*leap
+      # pnum = randint(5,100)
+      # inum = randint(10,500)
+      # steps = randint(10,500)
+
+      pnum = 4+i*2
+      steps = 20*(j+1)
+
+      px = zeros(pnum,'float')
+      py = linspace(-1,1,pnum,'float')*leap
       path = column_stack((px, py))
+
 
       s = GuidedSandSpline(
           guide,
           path,
           inum=INUM,
-          steps=STEPS,
+          steps=steps,
           stp=STP
           )
-      yield i, s
+      yield itt, s
 
 
 def main():
@@ -79,19 +82,16 @@ def main():
   from render.render import Animate
   from fn import Fn
 
-  import sys, traceback
-
 
   fn = Fn(prefix='./res/', postfix='.png')
-
   si = spline_iterator()
 
   def wrap(render):
+    import sys, traceback
     try:
       i, spline = si.next()
       print(i)
       for xy in spline:
-        # print(xy)
         draw(render, xy)
       return True
     except StopIteration:
