8ad245c0a84c9dd83a1d3b262683ce8b1ef476a0
Anders Hoff <inconvergent@gmail.com>
2016-07-25 23:19:22 +0200

    single vertical line

---
+++ main-vline.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from numpy.random import randint
+from numpy import linspace
+from numpy import arange
+from numpy import zeros
+from numpy import column_stack
+from numpy import pi
+from numpy import array
+
+BG = [1,1,1,1]
+FRONT = [0,0,0,0.05]
+
+
+TWOPI = 2.0*pi
+
+BACK = [1,1,1,1]
+FRONT = [0,0,0,0.01]
+
+SIZE = 13000
+
+EDGE = 0.08
+
+INUM = 5*SIZE
+
+STP = 0.00000003
+
+GAMMA = 1.5
+
+
+def f():
+  for x in linspace(EDGE, 1.0-EDGE, SIZE*5):
+    yield array([[x, 0.0]])
+
+def spline_iterator():
+  from modules.sandSpline import SandSpline
+
+  splines = []
+  guide = f()
+
+  pnum = randint(4,100)
+  px = zeros(pnum,'float')
+  py = linspace(EDGE, 1.0-EDGE, pnum)
+  path = column_stack([px,py])
+
+  scale = arange(pnum).astype('float')*STP
+
+  s = SandSpline(
+      guide,
+      path,
+      INUM,
+      scale
+      )
+  splines.append(s)
+
+  itt = 0
+  while True:
+    for w, s in enumerate(splines):
+      xy = next(s)
+      itt += 1
+      yield itt, w, xy
+
+
+def main():
+  import sys, traceback
+  from fn import Fn
+  from sand import Sand
+
+  sand = Sand(SIZE)
+  sand.set_bg(BG)
+  sand.set_rgba(FRONT)
+
+  fn = Fn(prefix='./res/', postfix='.png')
+  si = spline_iterator()
+
+  while True:
+    try:
+      itt, w, xy = next(si)
+      sand.paint_dots(xy)
+      if not itt%(SIZE):
+        print(itt)
+      #   sand.write_to_png(fn.name(), GAMMA)
+    except Exception as e:
+      print(e)
+      sand.write_to_png(fn.name(), GAMMA)
+      traceback.print_exc(file=sys.stdout)
+      return
+
+
+if __name__ == '__main__':
+  main()
+
